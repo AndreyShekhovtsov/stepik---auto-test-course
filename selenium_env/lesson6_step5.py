498f04003627c34774a28c9e3e0d1ebbcd2569bb
AndreyShekhovtsov <shekhovtsovandrey@mail.ru>
2021-04-02 12:07:47 +0300

    add all files

---
+++ selenium_env/lesson6_step5.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+import time
+import math
+
+link = "http://suninjuly.github.io/find_link_text"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    num = str(math.ceil(math.pow(math.pi, math.e)*10000))
+    button = browser.find_element_by_partial_link_text(num)
+    button.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
